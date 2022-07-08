b9ae89fa5600b86cbd3ec27c687436765fc9458e
Ben Cresswell <ben@hypermint.com>
2022-07-08 16:18:25 +0100

    Added goerli to networks enum

--- src/types/Enums.ts
+++ src/types/Enums.ts
@@ -1,31 +1,32 @@
 export enum TransactionStatus {
-  Pending = "Pending",
-  Complete = "Complete",
-  Failed = "Failed"
+    Pending = 'Pending',
+    Complete = 'Complete',
+    Failed = 'Failed'
 }
 
 export enum NetworkType {
-  Ethereum = "Ethereum",
-  Polygon = "Polygon",
-  Solana = "Solana"
+    Ethereum = 'Ethereum',
+    Polygon = 'Polygon',
+    Solana = 'Solana'
 }
 
 export enum NetworkEnvironment {
-  Emulator = "Emulator",
-  Testnet = "Testnet",
-  Mainnet = "Mainnet"
+    Emulator = 'Emulator',
+    Testnet = 'Testnet',
+    Mainnet = 'Mainnet'
 }
 
 export enum NFTContractType {
-  ERC721 = "ERC721",
-  ERC1155 = "ERC1155"
+    ERC721 = 'ERC721',
+    ERC1155 = 'ERC1155'
 }
 
 export enum NetworkChain {
-  EVMLocal = 1337,
-  Ethereum = 1,
-  Ropsten = 3,
-  Rinkeby = 4,
-  Polygon = 137,
-  Mumbai = 80001
+    EVMLocal = 1337,
+    Ethereum = 1,
+    Ropsten = 3,
+    Rinkeby = 4,
+    Goerli = 5,
+    Polygon = 137,
+    Mumbai = 80001
 }
