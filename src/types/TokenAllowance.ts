2721e273b460378bf5262499c4e44078d743086a
Jono Kercher <jonathan@hypermint.com>
2022-07-29 08:42:23 +0100

    remove comments

--- src/types/TokenAllowance.ts
+++ src/types/TokenAllowance.ts
@@ -1,8 +1,8 @@
 export interface TokenAllowance {
-    maxAllocation: number; // Total number of tokens a user can buy
-    maxAllocationPrice: number; // The price if they want all the tokens
+    maxAllocation: number;
+    maxAllocationPrice: number;
     breakdown: {
-        remainingAllocation: number; // Amount available at this step
+        remainingAllocation: number;
         pricePerToken: number;
     }[];
 }
