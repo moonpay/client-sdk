5a78cb5cbb197289272ecf80a223cbfeadb88143
Jono Kercher <jonathan@hypermint.com>
2022-08-26 15:55:47 +0100

    add all testnet nodes for wallet connect

--- src/wallets/WalletConnectWallet.ts
+++ src/wallets/WalletConnectWallet.ts
@@ -14,8 +14,10 @@ export default class WalletConnectWallet implements IWallet {
         try {
             const walletConnectProvider = new WalletConnectProvider({
                 rpc: {
-                    // TODO: add other providers
+                    1: 'https://eth-mainnet.g.alchemy.com/v2/jJF7ralq1qiM_ppY8x3fMwPWaSO2DVdx',
+                    137: 'https://polygon-mainnet.g.alchemy.com/v2/hzqvgVHD2lGOqvVZoA5FrKnEYXLzlA2N',
                     5: 'https://eth-goerli.g.alchemy.com/v2/PLCh4QRFSaauIUqazgnZ97NsgKYHYeJr',
+                    80001: 'https://polygon-mumbai.g.alchemy.com/v2/hfrnKzuPTK0Wm2vfXNCOS7LBTSICYsTX',
                     1337: 'http://localhost:8545'
                 },
                 chainId: this.config.networkChain
