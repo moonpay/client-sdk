1c7f1f0d18cc5738f8d969aa815aba5b538fea26
Jono Kercher <jonathan@hypermint.com>
2022-10-07 15:37:19 +0100

    PLAT-537 add mobile support for metamask and coinbase wallet

--- src/providers/MetaMaskWalletProvider.ts
+++ src/providers/MetaMaskWalletProvider.ts
@@ -2,6 +2,8 @@ import { ethers } from 'ethers';
 import WalletProvider from './WalletProvider';
 
 export default class MetaMaskWalletProvider extends WalletProvider {
+    private readonly deeplinkURL = `https://metamask.app.link/dapp/${window.location.host}`;
+
     public async getWeb3Provider() {
         let metamaskProvider: any = window?.ethereum?.isMetaMask
             ? window?.ethereum
@@ -14,9 +16,19 @@ export default class MetaMaskWalletProvider extends WalletProvider {
         }
 
         if (!metamaskProvider) {
+            if (this.isMobile()) {
+                this.deepLinkToWalletApp();
+
+                return;
+            }
+
             this.logger.log('getProvider', 'MetaMask wallet not found', true);
         }
 
         return new ethers.providers.Web3Provider(metamaskProvider);
+    }
+
+    private deepLinkToWalletApp() {
+        window.open(this.deeplinkURL, '_blank');
     }
 }
