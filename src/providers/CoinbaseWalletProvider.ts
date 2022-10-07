9578e9ae3de95e21d1a00fec4f95906e59cea551
jono-hypermint <109968961+jono-hypermint@users.noreply.github.com>
2022-10-07 17:29:03 +0100

    PLAT-537 add mobile support for metamask and coinbase wallet (#13)

--- src/providers/CoinbaseWalletProvider.ts
+++ src/providers/CoinbaseWalletProvider.ts
@@ -2,6 +2,8 @@ import { ethers } from 'ethers';
 import WalletProvider from './WalletProvider';
 
 export default class CoinbaseWalletProvider extends WalletProvider {
+    private readonly deeplinkURL = `https://go.cb-w.com/dapp?cb_url=${window.location.host}`;
+
     public async getWeb3Provider() {
         let coinbaseProvider: any = window?.ethereum?.isCoinbaseWallet
             ? window?.ethereum
@@ -14,9 +16,19 @@ export default class CoinbaseWalletProvider extends WalletProvider {
         }
 
         if (!coinbaseProvider) {
+            if (this.isMobile()) {
+                this.deepLinkToWalletApp();
+
+                return;
+            }
+
             this.logger.log('getProvider', 'Coinbase wallet not found', true);
         }
 
         return new ethers.providers.Web3Provider(coinbaseProvider);
+    }
+
+    private deepLinkToWalletApp() {
+        window.open(this.deeplinkURL, '_blank');
     }
 }
--- src/providers/WalletProvider.ts
+++ src/providers/WalletProvider.ts
@@ -6,4 +6,22 @@ export default abstract class WalletProvider implements IWalletProvider {
     constructor(protected readonly logger: Logger) {}
 
     abstract getWeb3Provider(): Promise<Web3Provider>;
+
+    protected isAndroid(): boolean {
+        return (
+            typeof navigator !== 'undefined' &&
+            /android/i.test(navigator.userAgent)
+        );
+    }
+
+    private isSmallIOS(): boolean {
+        return (
+            typeof navigator !== 'undefined' &&
+            /iPhone|iPod/.test(navigator.userAgent)
+        );
+    }
+
+    public isMobile(): boolean {
+        return this.isAndroid() || this.isSmallIOS();
+    }
 }
