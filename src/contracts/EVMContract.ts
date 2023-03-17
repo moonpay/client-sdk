ff9e259411322ccfd1ac7dcaeee893ffd0e4a566
Jay Jie <126987566+moonpayjay@users.noreply.github.com>
2023-03-17 13:49:56 -0700

    [HMINT-388] Don't Log Token Id for ER721 Contracts (#29)
    chore: don't log token id for ER721 contracts

--- src/contracts/EVMContract.ts
+++ src/contracts/EVMContract.ts
@@ -145,7 +145,9 @@ export class EVMContract extends BaseContract implements IContract {
 
         let balance;
 
-        if (this.config.contractType === NFTContractType.ERC721) {
+        const isERC721 = this.config.contractType === NFTContractType.ERC721;
+
+        if (isERC721) {
             balance = await contract.balanceOf(await signer.getAddress());
         } else {
             if (tokenId === undefined) {
@@ -166,7 +168,9 @@ export class EVMContract extends BaseContract implements IContract {
         const result = balance.toNumber();
         this.logger.log(
             'getTokenBalance',
-            `Token balance of ${tokenId}: ${result}`
+            isERC721
+                ? `Token balance: ${result}`
+                : `Token balance of ${tokenId}: ${result}`
         );
         return result;
     }
